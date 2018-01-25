236c27864793584dc9bead52e38d346d7ce5a7a2
byung-u <iam.byungwoo@gmail.com>
2018-01-25 11:42:18 +0900

    Add error exception

--- blog_posting/blog_post.py
+++ blog_posting/blog_post.py
@@ -242,10 +242,19 @@ class BlogPost:
 
     def translate_text(self, text, src='en', dest='ko'):
         if src is not 'ja':
-            ja_text = self.t.translate(text, src=src, dest='ja').text
-            ko_text = self.t.translate(ja_text, src='ja', dest=dest).text
+            try:
+                ja_text = self.t.translate(text, src=src, dest='ja').text
+            except json.decoder.JSONDecodeError:
+                return None
+            try:
+                ko_text = self.t.translate(ja_text, src='ja', dest=dest).text
+            except json.decoder.JSONDecodeError:
+                return None
         else:
-            ko_text = self.t.translate(text, src=src, dest=dest).text
+            try:
+                ko_text = self.t.translate(text, src=src, dest=dest).text
+            except json.decoder.JSONDecodeError:
+                return None
         return ko_text
 
     def translate_text_list(self, article, src='en', dest='ko'):
